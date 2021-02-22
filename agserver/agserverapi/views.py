4542f970bb984d6f93412b062664730d2e4d90d7
Austin Phy <atphy42@gmail.com>
2021-02-21 18:03:39 -0600

    limited search to only parking garages OBVIOUSLY

--- agserver/agserverapi/views.py
+++ agserver/agserverapi/views.py
@@ -8,13 +8,15 @@ class LocationSearch(ViewSet):
         
         yelp_api = YelpAPI(api_key)
         search_location = self.request.query_params.get("location", None)
-        search_total = yelp_api.search_query(location = search_location, sort_by='rating', limit=0)
+        search_total = yelp_api.search_query(location = search_location, categories='parking, All', sort_by='rating', limit=0)
         
-        if search_total["total"] > 1000:
+        if search_total["total"] <= 50:
+            offset_amt = 0
+        elif search_total["total"] <= 1000:
             offset_amt = 950
         else:
             offset_amt = (search_total["total"] - 50)
 
-        search_results = yelp_api.search_query(location = search_location, sort_by='rating', limit=50, offset=offset_amt)
+        search_results = yelp_api.search_query(location = search_location, categories='parking, All', sort_by='rating', limit=50, offset=offset_amt)
 
-        return Response(search_results["businesses"])
+        return Response(search_total["businesses"])
